99ecefb1d335f8007b3de0a0c6df7d8aa0b2873c
Philipe Morais <liperapltda@gmail.com>
2024-10-04 00:28:39 -0300

    feat(fastify): init a fastify server, prisma client and env variables

---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+const envSchema = z.object({
+  POSTGRES_PRISMA_URL: z.string().url(),
+  POSTGRES_URL_NON_POOLING: z.string().url(),
+  PORT: z.coerce.number(),
+  JWT_SECRET: z.string(),
+  COOKIE_SECRET: z.string(),
+})
+
+export const env = envSchema.parse(process.env)
---
+++ src/http/index.ts
@@ -0,0 +1,117 @@
+import fastify, { type FastifyReply, type FastifyRequest } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import fastifyCors from '@fastify/cors'
+import { env } from '../env'
+import fjwt, { type FastifyJWT } from '@fastify/jwt'
+import fCookie from '@fastify/cookie'
+import { getUsersRoute } from '../routes/Users/get-users'
+import { registerUserRoute } from '../routes/Users/user-register'
+import { userLoginRoute } from '../routes/Users/user-login'
+import { userLogoutRoute } from '../routes/Users/user-logout'
+import {
+  getAllProjectsRoute,
+  getProjectByIDRoute,
+} from '../routes/Projects/get-project'
+import { getAllTechsRoute, getTechByIDRoute } from '../routes/Techs/get-tech'
+import {
+  deleteAllProjectsRoute,
+  deleteProjectByIdRoute,
+} from '../routes/Projects/delete-project'
+import {
+  deleteAllTechsRoute,
+  deleteTechByIdRoute,
+} from '../routes/Techs/delete-tech'
+import { createTechRoute } from '../routes/Techs/create-tech'
+import { updateTechRoute } from '../routes/Techs/update-tech'
+import { createProjectRoute } from '../routes/Projects/create-project'
+import { updateProjectRoute } from '../routes/Projects/update-project'
+import { updateTechOrderRoute } from '../routes/ProjectTech/order'
+
+const listeners = ['SIGINT', 'SIGTERM']
+// biome-ignore lint/complexity/noForEach: <explanation>
+listeners.forEach(signal => {
+  process.on(signal, async () => {
+    await app.close()
+    process.exit(0)
+  })
+})
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Cors
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+// JWT
+app.register(fjwt, {
+  secret: env.JWT_SECRET,
+})
+app.addHook('preHandler', (req, res, next) => {
+  req.jwt = app.jwt
+  return next()
+})
+app.decorate(
+  'authenticate',
+  async (req: FastifyRequest, reply: FastifyReply) => {
+    const token = req.cookies.access_token
+
+    if (!token) {
+      return reply.status(401).send({ message: 'Unauthorized' })
+    }
+
+    const decoded = req.jwt.verify<FastifyJWT['user']>(token)
+    req.user = decoded
+  }
+)
+
+// Cookies
+app.register(fCookie, {
+  secret: env.COOKIE_SECRET,
+  hook: 'preHandler',
+})
+
+// Validator and Serializers ZOD
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+// Routes
+app.get('/', () => 'Hello World!')
+
+// Route Users
+app.register(getUsersRoute)
+app.register(registerUserRoute)
+app.register(userLoginRoute)
+app.register(userLogoutRoute)
+
+// Route Techs
+app.register(getAllTechsRoute)
+app.register(getTechByIDRoute)
+app.register(deleteAllTechsRoute)
+app.register(deleteTechByIdRoute)
+app.register(createTechRoute)
+app.register(updateTechRoute)
+
+// Route Projects
+app.register(getAllProjectsRoute)
+app.register(getProjectByIDRoute)
+app.register(deleteAllProjectsRoute)
+app.register(deleteProjectByIdRoute)
+app.register(createProjectRoute)
+app.register(updateProjectRoute)
+
+// Route ProjectsTech
+app.register(updateTechOrderRoute)
+
+// Server
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`HTTP Server running on http://localhost:${env.PORT}`)
+  })
