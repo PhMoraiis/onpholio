a76053015e0a73b8da3a1d2d94cfce869c0560fa
Philipe Morais <liperapltda@gmail.com>
2025-01-29 00:04:35 -0300

    fix(2fa): removing unused dependencies and remove feat 2fa

--- src/services/Users/create-user.ts
+++ src/services/Users/create-user.ts
@@ -1,8 +1,6 @@
 import type { FastifyReply } from 'fastify'
 import bcryptjs from 'bcryptjs'
 import { Prisma } from '@/database/index'
-import speakeasy from 'speakeasy'
-import QRCode from 'qrcode'
 
 interface CreateUserRequest {
   name?: string
@@ -29,32 +27,17 @@ export async function createUser(
   try {
     const hash = await bcryptjs.hash(password, 12)
 
-    // Gerar chave secreta para 2FA
-    const secret = speakeasy.generateSecret({ length: 20 })
-
-    // Verificar se o 'otpauth_url' está presente
-    if (!secret.otpauth_url) {
-      return reply.code(500).send({
-        message: 'Failed to generate OTP auth URL for 2FA',
-      })
-    }
-
-    // Gerar QR code para o app autenticador
-    const qrCodeUrl = await QRCode.toDataURL(secret.otpauth_url)
-
     const result = await Prisma.user.create({
       data: {
         name,
         email,
         password: hash,
-        twoFactorSecret: secret.base32,
       },
     })
 
     return reply.code(201).send({
       message: 'User created successfully',
       user: result,
-      twoFactorQrCode: qrCodeUrl, // Retornar o QR code para configuração do 2FA
     })
   } catch (error) {
     console.error(error)
